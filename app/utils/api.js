5a864a73f191cfda50d1b25b18322f6f2bfadb82
Filipsuper <filipkarlberg1@gmail.com>
2025-02-28 18:23:41 +0700

    test

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,11 +1,8 @@
-import axios from "axios";
-
 const API_URL = import.meta.env.VITE_API_URL;
-
 
 export async function fetchArticle() {
     try {
-        const response = await axios.get(`${API_URL}/data`);
+        const response = await fetch(`${API_URL}/data`);
         return response.data;
     } catch (error) {
         console.error('Error fetching data:', error);
