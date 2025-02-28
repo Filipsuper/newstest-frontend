ceff52d5487e9dcd8176bcadd4cd3c587d4f5a7b
Filipsuper <filipkarlberg1@gmail.com>
2025-02-28 17:34:51 +0700

    fix bug

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,19 +1,15 @@
 import axios from "axios";
-import { configDotenv } from "dotenv";
-configDotenv();
 
-const API_URL = process.env.API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 axios.defaults.baseURL = API_URL;
+
+console.log("API_URL", API_URL);
 
 export async function fetchArticle() {
     try {
         const response = await axios.get("/data");
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        return data;
+        return response.data;
     } catch (error) {
         console.error('Error fetching data:', error);
         throw error;
