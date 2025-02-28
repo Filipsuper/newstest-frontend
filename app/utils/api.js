fe601fa19adc9fc3ebad4e07e787117a994d5a94
Filipsuper <filipkarlberg1@gmail.com>
2025-02-28 17:36:48 +0700

    fix bug

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,13 +2,10 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-axios.defaults.baseURL = API_URL;
-
-console.log("API_URL", API_URL);
 
 export async function fetchArticle() {
     try {
-        const response = await axios.get("/data");
+        const response = await axios.get(`${API_URL}/data`);
         return response.data;
     } catch (error) {
         console.error('Error fetching data:', error);
