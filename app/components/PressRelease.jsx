046f034c77ea0b0b554c70b11e4de292726e6dd2
Filipsuper <filipkarlberg1@gmail.com>
2025-03-03 10:02:46 +0700

    small fix

--- app/components/PressRelease.jsx
+++ app/components/PressRelease.jsx
@@ -5,9 +5,12 @@ export default function PressRelease({ release }) {
     const [show, setShow] = useState()
 
     return (
-        <div className="mb-4 min-w-96 bg-foreground p-4 border border-border border-opacity-10 cursor-pointer  hover:brightness-150" onClick={() => { setShow(!show) }}>
-            <div className="w-full flex justify-between items-center">
+        <div className="mb-4 w-full md:min-w-96 bg-foreground p-4 border border-border border-opacity-10 cursor-pointer  hover:brightness-150" onClick={() => { setShow(!show) }}>
+            <div className="w-full flex gap-2 items-center">
                 <span>{release.ticker}</span>
+                <span>•</span>
+                <span>{release.time}</span>
+                <div className="flex flex-grow"></div>
                 <span className={"p-1 px-2 rounded-sm bg-background  " + importanceColor(release.importance)}>{release.importance}</span>
             </div>
             <h3 className="text-lg font-serif font-bold text-text italic mb-2">{release.title}</h3>
