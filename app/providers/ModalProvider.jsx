048538d071bf17b0861e19a383a915f179ccbc07
Filipsuper <filipkarlberg1@gmail.com>
2025-04-10 14:06:41 +0200

    better modal

--- app/providers/ModalProvider.jsx
+++ app/providers/ModalProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext } from 'react';
-import { FaCircleXmark } from "react-icons/fa6";
+import { FaX } from "react-icons/fa6";
 
 const ModalContext = createContext();
 
@@ -20,9 +20,9 @@ export const ModalProvider = ({ children }) => {
         <ModalContext.Provider value={{ modalContent, openModal, closeModal }}>
             {children}
             {modalContent && (
-                <div className="absolute top-0 left-0 right-0 flex h-full w-full items-center justify-center bg-[#0000003d] z-50">
-                    <div className="bg-background px-12 py-12 shadow-xl  flex justify-center items-center relative border border-border">
-                        <button onClick={closeModal} className="absolute top-2 right-2 font-sans text-secondary"><FaCircleXmark /></button>
+                <div className="absolute top-0 left-0 right-0 flex h-full w-full items-center justify-center bg-[#0000005d] z-50">
+                    <div className="bg-background py-12 px-20 shadow-xl  flex justify-center items-center relative border border-border">
+                        <button onClick={closeModal} className="absolute  top-2 right-2 text-text-muted font-sans text-xs "><FaX /></button>
                         {modalContent}
                     </div>
                 </div>
