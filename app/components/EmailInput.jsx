4dd7763b681ade41f84de08af1ebedae37ce18a5
Filipsuper <filipkarlberg1@gmail.com>
2025-05-04 22:53:56 +0200

    make email input less cluttered

--- app/components/EmailInput.jsx
+++ app/components/EmailInput.jsx
@@ -37,7 +37,7 @@ export default function EmailInput({ centered }) {
         <>
 
             <div className="flex flex-col max-w-md  font-sans group py-2 md:py-0">
-                <span className={"text-base text-text-article  pr-2 flex mb-2 w-full " + (centered ? "text-center" : "text-start")}>Missa inte nästa utskick:</span>
+                {/* <span className={"text-base text-text-article  pr-2 flex mb-2 w-full " + (centered ? "text-center" : "text-start")}>Missa inte nästa utskick:</span> */}
                 <form onSubmit={handleSubmit} className="flex flex-row items-center  gap-2">
                     <input id="mail" name="mail" className="border hover:border-white text-text outline-none w-5/6 md:w-full px-4 py-2 md:text-sm border-r border-border placeholder:text-text-muted focus:border-white" placeholder="Din email" />
                     <button type="submit" className="text-secondary hover:bg-secondary hover:text-background transition-colors duration-500 border px-4 py-2 border-secondary bg-foreground text-sm md:text-sm hover:cursor-pointer active:text-text" >Skicka</button>
