4251c6b8bc49b3aa84f777d3418b983cbc559844
Filipsuper <filipkarlberg1@gmail.com>
2025-10-09 11:45:01 +0200

    new text

--- app/routes/about.jsx
+++ app/routes/about.jsx
@@ -33,7 +33,7 @@ export default function About() {
                 <div className="shadow-md absolute left-2 md:-left-0 top-2 w-4 h-4 bg-background border border-border rounded-full text-xs flex flex-row justify-center items-center text-text-muted">4</div>
                 <h2 className="text-2xl font-semibold mb-4 text-text">Kontinuerlig förbättring</h2>
                 <p className="text-text-muted mb-4">
-                    Jag arbetar ständigt med att förbättra datainsamlingen och analysprocessen samt lägga till nya funktioner. Din feedback är ovärderlig för att göra detta nyhetsbrev ännu bättre.
+                    Jag arbetar ständigt med att förbättra datainsamlingen och analysprocessen samt lägga till nya funktioner. Din feedback är ovärderlig för att göra detta nyhetsbrev ännu bättre. OBS: Ingenting av artiklarna ska ses som rekommendationer
                 </p>
                 <span className="text-text-muted">Vid funderingar eller feedback, maila mig gärna på: <a href="mailto:filipkarlberg1@gmail.com" className="text-primary underline mt-4">filipkarlberg1@gmail.com</a></span>
             </section>
